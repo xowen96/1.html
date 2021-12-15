3fcba1806201bc16b054a9d115c0cef555e281ca
xowen96 <96163095+xowen96@users.noreply.github.com>
2021-12-15 14:14:26 +0900

    HTML v1

---
+++ script.js
@@ -0,0 +1,13 @@
+alert('hi');
+
+var id = prompt("ID를 입력하세요:");
+var pw = prompt("Type down the password: ");
+
+if((id === 'lol0601' || 'xowen96' || 'llvvll8686' )&& pw === 'fksl1250!!'){
+    alert(id + "is logged in ");
+}
+else{
+    alert("you have put the wrong ID or password!!!");
+   var id = prompt("type down Id once again");
+   var pw = prompot("type down password once again");
+}
